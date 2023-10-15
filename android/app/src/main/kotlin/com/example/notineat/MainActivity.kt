b35f8ff9bd31c64e424c120ee3bee53a3e8b75b2
Nilesh Kumar <nilesh.kumar@umma.id>
2023-10-15 16:51:29 +0400

    https://trello.com/c/UPwEErZx Make UI more elegant and colorful

--- android/app/src/main/kotlin/com/example/notineat/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.notineat
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/notineat/MainActivity.kt
@@ -0,0 +1,10 @@
+/*
+ * Copyright © [2023] [Nilesh Kumar]. All rights reserved.
+ *
+ */
+
+package com.notineat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
